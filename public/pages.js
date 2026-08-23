9de827cb90757b655e7d238967bebbd47b41535c
Tawfeeq <tawfeeq.tech@gmail.com>
2026-08-23 12:38:20 +0300

    chore: rebuild public assets

--- public/pages.js
+++ public/pages.js
@@ -1,1 +1,128 @@
-(()=>{"use strict";var e={194(e,t,n){class a{constructor(){}onReady(){}registerEvents(){}initiate(e){if(e&&!e.includes(salla.config.get("page.slug")))return app.log(`The Class For (${e.join(",")}) Skipped.`);this.onReady(),this.registerEvents(),app.log(`The Class For (${e?.join(",")||"*"}) Loaded🎉`)}}a.initiateWhenReady=function(e=null){"ready"===window.app?.status?(new this).initiate(e):document.addEventListener("theme::ready",()=>(new this).initiate(e))};const r=a;n.d(t,["A",0,r])}};const t={};function n(a){const r=t[a];if(void 0!==r)return r.exports;const i=t[a]={exports:{}};return e[a](i,i.exports,n),i.exports}n.d=(e,t)=>{if(Array.isArray(t))for(var a=0;a<t.length;){var r=t[a++],i=t[a++];n.o(e,r)?0===i&&a++:0===i?Object.defineProperty(e,r,{enumerable:!0,value:t[a++]}):Object.defineProperty(e,r,{enumerable:!0,get:i})}else for(var r in t)n.o(t,r)&&!n.o(e,r)&&Object.defineProperty(e,r,{enumerable:!0,get:t[r]})},n.o=(e,t)=>Object.prototype.hasOwnProperty.call(e,t);var a=n(194);class r extends a.A{onReady(){let e=app.element(".count-anime")?.dataset?.count||0;(new anime.timeline).add({targets:".loyality-item",opacity:[0,1],translateX:[20,0],delay:function(e,t){return 100*t}}).add({targets:".star-anime",opacity:[0,1],rotate:[50,0],duration:4e3,delay:function(e,t){return 100*t}},"-=1000").add({targets:".count-anime",innerText:[0,e],duration:2e3,easing:"linear",round:!0,delay:function(e,t){return 150*t}},"-=3700").add({targets:".btn-anime",opacity:[0,1],duration:2e3,translateX:[20,0],delay:function(e,t){return 100*t}},"-=3200")}}r.initiateWhenReady(["loyalty"]);var i=n(194);class s extends i.A{onReady(){const e=document.querySelector("#brands-nav");document.querySelector(".brands-nav-wrap").style.height=e.clientHeight+"px",app.onClick(".brands-nav__item",({target:e})=>{app.all(".brands-nav__item",t=>app.toggleElementClassIf(t,"is-selected","unselected",()=>t==e))}),window.addEventListener("scroll",()=>{let e=window.pageYOffset<=200;app.toggleClassIf("#brands-nav","is-not-sticky","is-sticky",()=>e)})}}s.initiateWhenReady(["brands.index"])})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/assets/js/base-page.js"
+/*!************************************!*\
+  !*** ./src/assets/js/base-page.js ***!
+  \************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nclass BasePage {\n  constructor() {}\n  onReady() {\n    //\n  }\n  registerEvents() {\n    //\n  }\n\n  /**\r\n   * To avoid loading unwanted classes, unless it's wanted page\r\n   * @param {null|string[]} allowedPages\r\n   * @return {*}\r\n   */\n  initiate(allowedPages) {\n    if (allowedPages && !allowedPages.includes(salla.config.get('page.slug'))) {\n      return app.log(`The Class For (${allowedPages.join(',')}) Skipped.`);\n    }\n    this.onReady();\n    this.registerEvents();\n    app.log(`The Class For (${allowedPages?.join(',') || '*'}) Loaded🎉`);\n  }\n}\n\n/**\r\n * Because we merged multi classes into one file, there is no need to initiate all of them\r\n */\nBasePage.initiateWhenReady = function (allowedPages = null) {\n  if (window.app?.status === 'ready') {\n    new this().initiate(allowedPages);\n  } else {\n    document.addEventListener('theme::ready', () => new this().initiate(allowedPages));\n  }\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (BasePage);\n\n//# sourceURL=webpack://mudarrib-theme/./src/assets/js/base-page.js?\n}");
+
+/***/ },
+
+/***/ "./src/assets/js/brands.js"
+/*!*********************************!*\
+  !*** ./src/assets/js/brands.js ***!
+  \*********************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _base_page__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base-page */ \"./src/assets/js/base-page.js\");\n\nclass Brands extends _base_page__WEBPACK_IMPORTED_MODULE_0__[\"default\"] {\n  onReady() {\n    // set initial height;\n    const nav = document.querySelector('#brands-nav'),\n      navWrap = document.querySelector('.brands-nav-wrap');\n    navWrap.style.height = nav.clientHeight + 'px';\n    app.onClick('.brands-nav__item', ({\n      target: btn\n    }) => {\n      app.all('.brands-nav__item', el => app.toggleElementClassIf(el, 'is-selected', 'unselected', () => el == btn));\n    });\n    window.addEventListener('scroll', () => {\n      let scrolAtTop = window.pageYOffset <= 200;\n      app.toggleClassIf('#brands-nav', 'is-not-sticky', 'is-sticky', () => scrolAtTop);\n    });\n  }\n}\nBrands.initiateWhenReady(['brands.index']);\n\n//# sourceURL=webpack://mudarrib-theme/./src/assets/js/brands.js?\n}");
+
+/***/ },
+
+/***/ "./src/assets/js/loyalty.js"
+/*!**********************************!*\
+  !*** ./src/assets/js/loyalty.js ***!
+  \**********************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _base_page__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base-page */ \"./src/assets/js/base-page.js\");\n\nclass Loyalty extends _base_page__WEBPACK_IMPORTED_MODULE_0__[\"default\"] {\n  onReady() {\n    let count = app.element(\".count-anime\")?.dataset?.count || 0;\n    new anime.timeline().add({\n      targets: '.loyality-item',\n      opacity: [0, 1],\n      translateX: [20, 0],\n      delay: function (el, i) {\n        return i * 100;\n      }\n    }).add({\n      targets: '.star-anime',\n      opacity: [0, 1],\n      rotate: [50, 0],\n      duration: 4000,\n      delay: function (el, i) {\n        return i * 100;\n      }\n    }, '-=1000').add({\n      targets: `.count-anime`,\n      innerText: [0, count],\n      duration: 2000,\n      easing: \"linear\",\n      round: true,\n      delay: function (el, i) {\n        return i * 150;\n      }\n    }, '-=3700').add({\n      targets: '.btn-anime',\n      opacity: [0, 1],\n      duration: 2000,\n      translateX: [20, 0],\n      delay: function (el, i) {\n        return i * 100;\n      }\n    }, '-=3200');\n  }\n}\nLoyalty.initiateWhenReady(['loyalty']);\n\n//# sourceURL=webpack://mudarrib-theme/./src/assets/js/loyalty.js?\n}");
+
+/***/ }
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	const __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		const cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		const module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		if (!(moduleId in __webpack_modules__)) {
+/******/ 			delete __webpack_module_cache__[moduleId];
+/******/ 			const e = new Error("Cannot find module '" + moduleId + "'");
+/******/ 			e.code = 'MODULE_NOT_FOUND';
+/******/ 			throw e;
+/******/ 		}
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter/value functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			if(Array.isArray(definition)) {
+/******/ 				var i = 0;
+/******/ 				while(i < definition.length) {
+/******/ 					var key = definition[i++];
+/******/ 					var binding = definition[i++];
+/******/ 					if(!__webpack_require__.o(exports, key)) {
+/******/ 						if(binding === 0) {
+/******/ 							Object.defineProperty(exports, key, { enumerable: true, value: definition[i++] });
+/******/ 						} else {
+/******/ 							Object.defineProperty(exports, key, { enumerable: true, get: binding });
+/******/ 						}
+/******/ 					} else if(binding === 0) { i++; }
+/******/ 				}
+/******/ 			} else {
+/******/ 				for(var key in definition) {
+/******/ 					if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 						Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 					}
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	__webpack_require__("./src/assets/js/loyalty.js");
+/******/ 	let __webpack_exports__ = __webpack_require__("./src/assets/js/brands.js");
+/******/ 	
+/******/ })()
+;
--- public/wishlist-card.js
+++ public/wishlist-card.js
@@ -1,1 +1,32 @@
-(()=>{class t extends HTMLElement{connectedCallback(){if(!this.product)return salla.logger.warn("custom-wishlist-card:: product does not exist!");salla.onReady(()=>this.render())}render(){this.setAttribute("id",`wishlist-product-${this.product.id}`),this.classList.add("product-entry","product-entry--wishlist"),this.innerHTML=`\n        <div class="flex items-center mb-4 sm:mb-0">\n          <a href="${this.product.url}" class="product-entry__image">\n            <img class="object-cover w-full h-full" src="${this.product.image.url}" loading="lazy" alt="${this.product.image.alt}" />\n          </a>\n          <div class="flex-1 rtl:pr-5 ltr:pl-5">\n            <h3 class="text-sm text-gray-800 leading-6 mb-1.5 rtl:pl-5 ltr:pr-5 rtl:md:pl-8 ltr:md:pr-8 line-clamp-1">\n              <a href="${this.product.url}">${this.product.name}</a>\n            </h3>\n            <div class="w-full center-between">\n              ${this.product.is_on_sale?`\n                <div class="space-x-1 rtl:space-x-reverse">\n                  <h4 class="inline-block text-sm font-bold text-red-400">${salla.money(this.product.sale_price)}</h4>\n                  <span class="text-sm text-gray-500 line-through">${salla.money(this.product.regular_price)}</span>\n                </div>\n              `:`\n                <h4 class="text-sm font-bold">${salla.money(this.product.price)}</h4>\n              `}\n            </div>\n          </div>\n        </div>\n        <div class="flex items-center space-x-4 rtl:space-x-reverse">\n          <salla-add-product-button product-status="${this.product.status}" product-id="${this.product.id}" product-type="${this.product.type}" loader-position="center" fill="outline" class="flex-grow w-full sm:grow-0 md:w-40">\n          </salla-add-product-button>\n          <salla-button loader-position="center" shape="icon" size="small" color="danger" class="btn--delete" onclick="salla.wishlist.remove(${this.product.id})">\n            <i class="sicon-cancel"></i>\n          </salla-button>\n        </div>\n  `}}customElements.define("custom-wishlist-card",t)})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/assets/js/partials/wishlist-card.js"
+/*!*************************************************!*\
+  !*** ./src/assets/js/partials/wishlist-card.js ***!
+  \*************************************************/
+() {
+
+eval("{class WishlistCard extends HTMLElement {\n  connectedCallback() {\n    if (!this.product) {\n      return salla.logger.warn('custom-wishlist-card:: product does not exist!');\n    }\n    salla.onReady(() => this.render());\n  }\n  render() {\n    this.setAttribute('id', `wishlist-product-${this.product.id}`);\n    this.classList.add('product-entry', 'product-entry--wishlist');\n    this.innerHTML = `\n        <div class=\"flex items-center mb-4 sm:mb-0\">\n          <a href=\"${this.product.url}\" class=\"product-entry__image\">\n            <img class=\"object-cover w-full h-full\" src=\"${this.product.image.url}\" loading=\"lazy\" alt=\"${this.product.image.alt}\" />\n          </a>\n          <div class=\"flex-1 rtl:pr-5 ltr:pl-5\">\n            <h3 class=\"text-sm text-gray-800 leading-6 mb-1.5 rtl:pl-5 ltr:pr-5 rtl:md:pl-8 ltr:md:pr-8 line-clamp-1\">\n              <a href=\"${this.product.url}\">${this.product.name}</a>\n            </h3>\n            <div class=\"w-full center-between\">\n              ${this.product.is_on_sale ? `\n                <div class=\"space-x-1 rtl:space-x-reverse\">\n                  <h4 class=\"inline-block text-sm font-bold text-red-400\">${salla.money(this.product.sale_price)}</h4>\n                  <span class=\"text-sm text-gray-500 line-through\">${salla.money(this.product.regular_price)}</span>\n                </div>\n              ` : `\n                <h4 class=\"text-sm font-bold\">${salla.money(this.product.price)}</h4>\n              `}\n            </div>\n          </div>\n        </div>\n        <div class=\"flex items-center space-x-4 rtl:space-x-reverse\">\n          <salla-add-product-button product-status=\"${this.product.status}\" product-id=\"${this.product.id}\" product-type=\"${this.product.type}\" loader-position=\"center\" fill=\"outline\" class=\"flex-grow w-full sm:grow-0 md:w-40\">\n          </salla-add-product-button>\n          <salla-button loader-position=\"center\" shape=\"icon\" size=\"small\" color=\"danger\" class=\"btn--delete\" onclick=\"salla.wishlist.remove(${this.product.id})\">\n            <i class=\"sicon-cancel\"></i>\n          </salla-button>\n        </div>\n  `;\n  }\n}\ncustomElements.define('custom-wishlist-card', WishlistCard);\n\n//# sourceURL=webpack://mudarrib-theme/./src/assets/js/partials/wishlist-card.js?\n}");
+
+/***/ }
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	let __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/assets/js/partials/wishlist-card.js"]();
+/******/ 	
+/******/ })()
+;
